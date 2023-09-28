f3b23c77b86658c0ee2642e97d40aedd63435298
antlas <anton_lastochkin@mail.ru>
2023-09-28 08:42:29 +0300

    upd text, remove batch file

--- gen_cmake_msvc2022.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-
-REM Сборка
-mkdir build
-cd build
-cmake -G "Visual Studio 17 2022" -A "x64" ..
-pause
